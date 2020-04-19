e0afb07cf260f8be90a66b40d398b3f251db53f2
Admin <zzzzzzshpig@163.com>
2020-04-19 10:38:41 +0800

    feat: 新增单例模式

---
+++ base/JavaScript/设计模式/单例模式.js
@@ -0,0 +1,21 @@
+class Vuex {
+	static install () {
+		// 一些install 操作
+		console.log('install成功')
+
+		Vuex.install = function () {
+			console.log('install失败，只能是单例。')
+		}
+	}
+}
+
+let Vue = {
+	use (fn) {
+		fn.install()
+	}
+}
+
+Vue.use(Vuex)
+Vue.use(Vuex)
+
+
