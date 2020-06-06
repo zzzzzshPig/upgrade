ec284ae06efdf44ddd1b5865290ce55ce9455d08
Admin <1012352205@qq.com>
2020-06-06 23:16:33 +0800

    JavaScript: 算法/排序 新增归并排序

--- JavaScript/算法和数据结构/排序.js
+++ JavaScript/算法和数据结构/排序.js
@@ -1,5 +1,12 @@
 const { getRandomNumber } = require('./utils')
 
+const test = []
+for (let i = 0; i < 10; i++) {
+    test.push(getRandomNumber(0, 10))
+}
+
+console.log('测试数据 ----- ', test)
+
 // 默认排序方式为由小到大的方式
 // 冒泡和选择这两个就不写了。。。
 
@@ -28,8 +35,33 @@ function insert (arr) {
     return arr
 }
 
-const test = []
-for (let i = 0; i < 100; i++) {
-    test.push(getRandomNumber(0, 100))
+// console.log(insert(test))
+
+// 双指针
+function mergeTwoYouXuArray (one, two) {
+    let i = 0
+    let j = 0
+    const res = []
+
+    while (i < one.length || j < two.length) {
+        if (one[i] > two[j] || one[i] === undefined) {
+            res.push(two[j])
+            j++
+        } else {
+            res.push(one[i])
+            i++
+        }
+    }
+
+    return res
 }
-console.log(insert(test))
+
+// 归并排序
+function guibin (arr) {
+    if (arr.length === 1) return arr
+    const len = Math.floor(arr.length / 2)
+
+    return mergeTwoYouXuArray(guibin(arr.slice(0, len)), guibin(arr.slice(len)))
+}
+
+console.log(guibin(test))
