00d2cad62b1e39b755557b3fa9d69b494ce8c7b8
Admin <1012352205@qq.com>
2020-06-08 23:31:14 +0800

    vue: init vue-demo

---
+++ Vue/vue-demo/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {}
+</script>
+
+<style lang="less" scoped></style>
---
+++ Vue/vue-demo/src/promise/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="promise"></div>
+</template>
+
+<script lang="ts" src="./index.ts"></script>
