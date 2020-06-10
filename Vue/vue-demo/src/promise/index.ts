ec743e12c8538ad91561c537acc572fae0ee13d8
Admin <1012352205@qq.com>
2020-06-10 19:58:47 +0800

    feat(vue): vue-demo promise优化

--- Vue/vue-demo/src/promise/index.ts
+++ Vue/vue-demo/src/promise/index.ts
@@ -12,15 +12,11 @@ interface PromiseLoading {
   timer: number;
 }
 
-// 每一次promise变动都会导致uid增加
-let uid = 0
-
+type promise = Promise<any> | null
 @Component
 export default class Promised extends Vue {
   @Watch('promise')
-  async watchPromise () {
-    // 缓存uid 在promise决策后进行判断
-    const id = ++uid
+  async watchPromise (promise: promise) {
     this.refreshStatus()
 
     // 传入的不是promise则既不resolve 也不reject
@@ -33,15 +29,15 @@ export default class Promised extends Vue {
       const data = await this.promise
 
       // id 和 uid不一致 则丢弃掉这次结果 - 因为在pending期间 promise的值发生了变化
-      if (id === uid) {
+      if (this.promise === promise) {
         this.resolvePromise(data)
       }
     } catch (e) {
-      if (id === uid) {
+      if (this.promise === promise) {
         this.rejectPromise(e)
       }
     } finally {
-      if (id === uid) {
+      if (this.promise === promise) {
         this.hideLoading()
       }
     }
