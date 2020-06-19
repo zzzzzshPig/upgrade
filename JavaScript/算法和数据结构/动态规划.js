4b912f0b08a587fcf8f103074e1bc8e16d56a6ac
周绍辉 <zzzzzzshpig@163.com>
2020-06-19 18:28:21 +0800

    Javascript: 算法/动态规划 新增不同路径2

--- JavaScript/算法和数据结构/动态规划.js
+++ JavaScript/算法和数据结构/动态规划.js
@@ -1,5 +1,4 @@
 // 最长上升子序列 https://leetcode-cn.com/problems/longest-increasing-subsequence/
-
 function lengthOfLIS (nums) {
 	if (nums.length === 0) return 0
 
@@ -22,7 +21,6 @@ function lengthOfLIS (nums) {
 }
 
 // 零钱兑换 https://leetcode-cn.com/problems/coin-change/
-
 function coinChange (coins, amount) {
 	// 状态转移方程 f(n) = Math.min(f(n - c1) + 1, f(n - cn) + 1)
 
@@ -41,4 +39,32 @@ function coinChange (coins, amount) {
 	if (f[amount] === Infinity) return -1
 
 	return f[amount]
+}
+
+// 不同路径 II https://leetcode-cn.com/problems/longest-palindromic-substring/
+function uniquePathsWithObstacles (obstacleGrid) {
+	if (!obstacleGrid.length) return 0
+
+	// f(n, m) = f(n - 1, m) + f(n, m - 1)
+	const left = []
+	for (let i = 0; i < obstacleGrid.length; i++) {
+		left.push((obstacleGrid[i][0] === 1 || left[i - 1] === 0) ? 0 : 1)
+	}
+
+	const dp = []
+	if (obstacleGrid[0]) {
+		for (let i = 0; i < obstacleGrid[0].length; i++) {
+			dp.push((obstacleGrid[0][i] === 1 || dp[i - 1] === 0) ? 0 : 1)
+		}
+	}
+
+	const len1 = obstacleGrid.length
+	const len2 = obstacleGrid[0].length
+	for (let i = 1; i < len1; i++) {
+		dp[0] = left[i]
+		for (let j = 1; j < len2; j++) {
+			dp[j] = obstacleGrid[i][j] === 1 ? 0 : dp[j] + dp[j - 1]
+		}
+	}
+	return dp[dp.length - 1]
 }
