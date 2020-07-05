5096d196a1876b71a033187b2e083c785acd3f36
Admin <1012352205@qq.com>
2020-07-05 22:36:06 +0800

    Vue: vue3 init vue3-webpack

--- Vue/vue3/vue3-webpack/src/components/HelloWorld.vue
+++ Vue/vue3/vue3-webpack/src/components/HelloWorld.vue
@@ -9,10 +9,7 @@
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
@@ -33,17 +30,24 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
-
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      default: '',
+      type: String
+    }
+  },
+  setup () {
+    return {
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="less">
+<style scoped>
 h3 {
   margin: 40px 0 0;
 }
