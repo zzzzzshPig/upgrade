2ba5f955f32b9b9a7c4c89e508084d8c2533d51a
Admin <1012352205@qq.com>
2020-07-05 22:04:12 +0800

    Vue: vue3 init vue3-webpack

--- Vue/vue3/vue3-webpack/src/components/HelloWorld.vue
+++ Vue/vue3/vue3-webpack/src/components/HelloWorld.vue
@@ -11,6 +11,8 @@
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
