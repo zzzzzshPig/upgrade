3a2acebc4141f584f19d05302a329bb10dcafba8
Admin <1012352205@qq.com>
2021-04-08 00:04:36 +0800

    Feat: 算法题新解 1470号题目新增原数组操作的解题思路

--- JavaScript/算法和数据结构/leetcode/1470. 重新排列数组.js
+++ JavaScript/算法和数据结构/leetcode/1470. 重新排列数组.js
@@ -12,3 +12,21 @@ var shuffle = function(nums, n) {
 
     return res
 };
+
+// 原数组上操作的解法
+var shuffle1 = function(nums, n) {
+    const y = n
+    let x = []
+
+    for (let i = 1; i < nums.length - 1; i++) {
+        if (i < y) x.push(nums[i])
+
+        if (i % 2 === 1) {
+            nums[i] = nums[n]
+        } else {
+            nums[i] = x.shift()
+            n++
+        }
+    }
+    return nums
+};
